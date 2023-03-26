c2efc732042b7282b8ac4b2ff5d0711abedda07e
CleavonWang <CleavonWang>
2023-03-26 11:29:51 -0700

    still stuck :(

--- street fighter.py
+++ street fighter.py
@@ -1,11 +1,10 @@
 import pygame
+import Draw
 
 pygame.init()
 
 
 regenerated = False
-laser = False
-fire  = False
 blade = False
 punch = False
 
@@ -18,7 +17,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
 # load background image
-bg_image = pygame.image.load("brawler game/Assets/images/background/background.jpg").convert_alpha()
+
 
 
 # load player images and resize them  (and laser)
@@ -32,16 +31,8 @@ player2_image = pygame.transform.scale(player2_image, (100, 100))
 player2_rect = player2_image.get_rect(midbottom=(3 * SCREEN_WIDTH // 4, SCREEN_HEIGHT - 10))
 player2_lives = 3
 
-laser_image = pygame.image.load("brawler game/Assets/images/laser.png").convert_alpha()
-laser_image = pygame.transform.scale(laser_image, (100,50))
-laser_rect = laser_image.get_rect(midbottom=(1000,1000))
-
-fire_image = pygame.image.load("brawler game/Assets/images/fire.png")
-fire_image = pygame.transform.scale(fire_image, (200,50))
-fire_rect = fire_image.get_rect(midbottom=(-1000,-1000))
-
 blade_image = pygame.image.load("brawler game/Assets/images/blade.png")
-blade_image = pygame.transform.scale(blade_image, (50,50))
+blade_image = pygame.transform.scale(blade_image, (100,100))
 blade_rect = blade_image.get_rect(midbottom=(-10000,-10000))
 
 punch_image = pygame.image.load("brawler game/Assets/images/punch.png")
@@ -77,13 +68,6 @@ def draw_players():
     screen.blit(player1_image, player1_rect)
     screen.blit(player2_image, player2_rect)
 
-#drawing laserbream player2 ability
-def draw_laser():
-    screen.blit(laser_image, laser_rect)
-
-#drawing fire player 1 ability
-def draw_fire():
-    screen.blit(fire_image, fire_rect)
 
 #drawing shuriken player 2 ability
 def draw_blade():
@@ -100,31 +84,14 @@ clock = pygame.time.Clock()
 while run:
     clock.tick(60)  # limit frame rate to 60 FPS
 
-    # draw background and players
-    draw_bg()
+    # draw backgroudpnd and players
+    Draw.bg_image()
 
     draw_player1()
     
         
     draw_player2()
 
-    draw_laser()
-    if laser == True:
-        laser_rect.move_ip(-20,0)
-
-        if laser_rect.colliderect(player1_rect):
-            player1_lives -= 1
-            laser_rect.midbottom = (1000, 1000)  # reset laser position
-            laser = False
-
-    draw_fire()
-    if fire == True:
-        fire_rect.move_ip(20,0)
-
-        if fire_rect.colliderect(player2_rect):
-            player2_lives -=1
-            fire_rect.midbottom = (-1000, -1000)
-            fire = False
 
     draw_blade()
     if blade == True:
@@ -164,9 +131,6 @@ while run:
         if blade_rect.colliderect(player1_rect):
             player1_lives -= 1
 
-        if laser_rect.colliderect(player1_rect):
-            player1_lives -= 1
-
     if player1_lives == 0:
         pygame.quit()
 
@@ -198,9 +162,6 @@ while run:
             player1_rect.move_ip(0, -10)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
             player1_rect.move_ip(0, 10)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            fire_rect.midbottom = player1_rect.midbottom
-            fire = True
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_m:
             punch_rect.midbottom = player1_rect.midbottom
             punch = True
@@ -214,9 +175,6 @@ while run:
             player2_rect.move_ip(0, -10)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             player2_rect.move_ip(0, 10)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_o:
-            laser_rect.midbottom = player2_rect.midbottom
-            laser = True
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
             blade_rect.midbottom=player2_rect.midbottom
             blade = True
